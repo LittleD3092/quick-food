a18fed9f540ceb2d106fcc252deb402d3e656c2f
LittleD3092 <drevild01801@gmail.com>
2022-03-09 22:55:17 +0800

    convert indentation to tabs

--- Alphabet-Recognize/camera_findcontours.py
+++ Alphabet-Recognize/camera_findcontours.py
@@ -4,25 +4,25 @@ import cv2
 cap = cv2.VideoCapture(0)
 
 while True:
-    # read from camera
-    #     ret: whether capture is successful or not
-    #     frame: picture captured
-    ret, frame = cap.read()
+	# read from camera
+	#     ret: whether capture is successful or not
+	#     frame: picture captured
+	ret, frame = cap.read()
 
-    # copy the frame
-    img2 = frame.copy()
+	# copy the frame
+	img2 = frame.copy()
 
-    # full color --> gray
-    gray = cv2.cvtColor(frame , cv2.COLOR_BGR2GRAY)
+	# full color --> gray
+	gray = cv2.cvtColor(frame , cv2.COLOR_BGR2GRAY)
 
-    # gray -- 150 ~ 200 --> canny
-    cny = cv2.Canny(gray ,150 ,200)
+	# gray -- 150 ~ 200 --> canny
+	cny = cv2.Canny(gray ,150 ,200)
 
-    # get contour data from canny
-    binary, contours, hier = cv2.findContours(cny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    
-    # draw contour
-    for cnt in contours:
-        cv2.drawContours(img2, cnt, -1, (255, 0, 0), 4)
-    cv2.imshow('img2', img2)
-    cv2.waitKey(1)
+	# get contour data from canny
+	binary, contours, hier = cv2.findContours(cny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+	
+	# draw contour
+	for cnt in contours:
+		cv2.drawContours(img2, cnt, -1, (255, 0, 0), 4)
+	cv2.imshow('img2', img2)
+	cv2.waitKey(1)
