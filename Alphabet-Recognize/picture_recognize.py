136f62952c5c60ea41ba93ad64be64cb9db2fd85
LittleD3092 <drevild01801@gmail.com>
2022-03-13 18:12:34 +0800

    try tesseract

--- Alphabet-Recognize/picture_recognize.py
+++ Alphabet-Recognize/picture_recognize.py
@@ -1,23 +1,30 @@
 import cv2
+# a text recognize library
+from pytesseract import pytesseract
+
+# command used in terminal to call tesseract
+# 	for Windows, "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+# 	for Linux, "tesseract"
+pytesseract.tesseract_cmd = "tesseract"
 
 # read image from file
-#img = cv2.imread('T.jpeg')
-img = cv2.imread('k.png')
-img2 = img.copy()
+#img = cv2.imread('T.png')
+#img = cv2.imread('k.png')
+img = cv2.imread('TDK.png')
+height, width, c = img.shape
 
-# image --> gray -- 150~200 --> canny
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-canny = cv2.Canny(img, 150, 200)
+# detect words in image and print
+words_in_image = pytesseract.image_to_string(img)
+print(words_in_image)
 
-# get contour data from canny
-binary, contours, hier = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+# detect words and boxes in image
+letter_boxes = pytesseract.image_to_boxes(img)
+#print(letter_boxes)
+for box in letter_boxes.splitlines():
+	box = box.split()
+	x, y, w, h = int(box[1]), int(box[2]), int(box[3]), int(box[4])
+	cv2.rectangle(img, (x, height - y), (w, height - h), (0, 0, 255), 3)
+	cv2.putText(img, box[0], (x, height - h + 32), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
 
-# draw contour
-for cnt in contours:
-	cv2.drawContours(img2, cnt, -1, (255, 0, 0), 4)
-	peri = cv2.arcLength(cnt, True)
-	vertices = cv2.approxPolyDP(cnt, peri * 0.02, True)
-	print(len(vertices))
-cv2.imshow('canny', canny)
-cv2.imshow('img2', img2)
+cv2.imshow("window", img)
 cv2.waitKey(0)
