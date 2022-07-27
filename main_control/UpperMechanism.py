1075a751a0685fa6ecce69ce2f9172ba1af504e3
LittleD3092 <drevild01801@gmail.com>
2022-07-27 17:43:00 +0800

    add the client node implementation UpperMechanism.py

--- main_control/UpperMechanism.py
+++ main_control/UpperMechanism.py
@@ -1,11 +1,15 @@
 # UpperMechanism.py
 # Class implementation for ROS client of server "upper_mechanism".
 
+import rospy
+from std_msgs.msg import Int16
+
 class UpperMechanism:
 
 	# Precondition: Nothing.
-	# Postcondition: Client is up.
+	# Postcondition: Nothing.
 	def __init__(self):
+		# intentionally empty
 		pass
 
 	# Precondition: Client is up. Given an integer 
@@ -16,4 +20,11 @@ class UpperMechanism:
 	# 				- 3 for take bowling.
 	#				- 4 for release bowling.
 	def move(self, cmd):
-		pass
+		rospy.wait_for_service('upper_mechanism', 5)
+		try:
+			upper_mechanism = rospy.ServiceProxy('upper_mechanism', Int16)
+			resp = upper_mechanism(cmd)
+			return resp.data
+		except rospy.ServiceException as e:
+			print("Service call failed: %s" %e)
+			return -1
